b0e30e7904e13b2ffc369a530fe63f86a41dc9bf
rihab-lab <rihab.bahri7@gmail.com>
2026-02-10 11:56:21 +0100

    correct account roles

--- terraform/snowflake/envs/dev/provider.tf
+++ terraform/snowflake/envs/dev/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.90"
+      source  = "snowflakedb/snowflake"
+      version = "~> 2.1"
     }
   }
 }
