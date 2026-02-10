ee4c8aff424ea156d4cf3bea780130793dfa4bad
rihab-lab <rihab.bahri7@gmail.com>
2026-02-10 11:33:04 +0100

    change ci

--- terraform/snowflake/modules/snowflake_base_env/versions.tf
+++ terraform/snowflake/modules/snowflake_base_env/versions.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "= 0.92.0"
+      source  = "snowflakedb/snowflake"
+      version = "0.92.0"
     }
   }
 }
