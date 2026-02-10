3e589b17d525523c2683e2b630a03332753e5733
rihab-lab <rihab.bahri7@gmail.com>
2026-02-10 11:07:46 +0100

    change version

--- terraform/snowflake/modules/snowflake_base_env/versions.tf
+++ terraform/snowflake/modules/snowflake_base_env/versions.tf
@@ -1,11 +1,9 @@
 #verrouiller les versions
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.90"
+      version = "= 0.92.0"
     }
   }
 }
