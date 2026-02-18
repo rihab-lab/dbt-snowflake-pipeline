06bb1fc82892076547e7e7c47ecd87fc13b5cc25
rihab-lab <rihab.bahri7@gmail.com>
2026-02-18 14:53:25 +0100

    CORRECT BACKEND

--- terraform/gcp/org_projects/versions.tf
+++ terraform/gcp/org_projects/versions.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.30"
+      version = "~> 5.0"
     }
   }
-
-  backend "gcs" {}
 }
