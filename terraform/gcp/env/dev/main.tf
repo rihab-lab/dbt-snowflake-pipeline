1a59048c6ce04c3c3be4bc21ee00bf88431fb009
rihab-lab <rihab.bahri7@gmail.com>
2026-02-23 15:16:25 +0100

    composer dev

--- terraform/gcp/env/dev/main.tf
+++ terraform/gcp/env/dev/main.tf
@@ -69,3 +69,92 @@ resource "google_storage_bucket_iam_member" "archive_admin" {
 
   depends_on = [google_storage_bucket.landing]
 }
+#composer
+# Active les APIs requises (Composer + dépendances)
+resource "google_project_service" "apis" {
+  for_each = toset([
+    "composer.googleapis.com",
+    "compute.googleapis.com",
+    "storage.googleapis.com",
+    "iam.googleapis.com",
+    "secretmanager.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+  ])
+
+  project            = local.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
+resource "google_service_account" "composer" {
+  project      = local.project_id
+  account_id   = "sa-composer-dev"
+  display_name = "Composer SA (dev)"
+  depends_on   = [google_project_service.apis]
+}
+
+# Donne accès lecture aux fichiers entrants
+resource "google_storage_bucket_iam_member" "landing_viewer_composer" {
+  bucket = google_storage_bucket.landing.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.composer.email}"
+}
+
+# Donne accès écriture pour archiver / déplacer
+resource "google_storage_bucket_iam_member" "archive_admin_composer" {
+  bucket = google_storage_bucket.archive.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.composer.email}"
+}
+
+# Lire les secrets (Snowflake/dbt) dans Secret Manager (si tu l'utilises)
+resource "google_project_iam_member" "composer_secret_accessor" {
+  project = local.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.composer.email}"
+}
+resource "google_composer_environment" "dev" {
+  project = local.project_id
+  name    = "composer-pipeone-dev"
+  region  = var.region
+
+  config {
+    node_config {
+      service_account = google_service_account.composer.email
+    }
+
+    # Composer 2
+    software_config {
+      image_version = "composer-2-airflow-2"
+    }
+
+    workloads_config {
+      scheduler {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 10
+        count      = 1
+      }
+
+      web_server {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 10
+      }
+
+      worker {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 10
+        min_count  = 1
+        max_count  = 2
+      }
+    }
+  }
+
+  depends_on = [
+    google_project_service.apis
+  ]
+}
