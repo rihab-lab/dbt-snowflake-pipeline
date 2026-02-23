d185b75e60bcfbaa0701b30a54c9d43fc452a7cd
rihab-lab <rihab.bahri7@gmail.com>
2026-02-23 14:58:12 +0100

    create bucket gcp

--- terraform/gcp/env/prod/main.tf
+++ terraform/gcp/env/prod/main.tf
@@ -11,3 +11,46 @@ module "project" {
     managed_by  = "terraform"
   }
 }
+
+# On récupère l'ID du projet créé
+locals {
+  project_id = module.project.projects_map["prod"]
+}
+
+# Bucket landing
+resource "google_storage_bucket" "landing" {
+  name                        = "bck-pipeone-landing-prod"
+  project                     = local.project_id
+  location                    = var.region
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition { age = 30 }
+    action    { type = "Delete" }
+  }
+
+  depends_on = [module.project]
+}
+
+# Bucket archive
+resource "google_storage_bucket" "archive" {
+  name                        = "bck-pipeone-archive-prod"
+  project                     = local.project_id
+  location                    = var.region
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition { age = 180 }
+    action    { type = "Delete" }
+  }
+
+  depends_on = [module.project]
+}
--- terraform/gcp/env/stg/variables.tf
+++ terraform/gcp/env/stg/variables.tf
@@ -1,3 +1,7 @@
 variable "bootstrap_project_id" { type = string }
 variable "org_id" { type = string }
 variable "billing_account_id" { type = string }
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
