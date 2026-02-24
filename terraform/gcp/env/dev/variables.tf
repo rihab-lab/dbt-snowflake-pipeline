54670149a749f902be8dbb3a8d61e44981e4af03
rihab-lab <rihab.bahri7@gmail.com>
2026-02-24 10:36:24 +0100

    deploy dag

--- terraform/gcp/env/dev/variables.tf
+++ terraform/gcp/env/dev/variables.tf
@@ -5,3 +5,7 @@ variable "region" {
   type    = string
   default = "europe-west1"
 }
+variable "ci_service_account_email" {
+  description = "Service Account utilisé par la CI pour déployer les DAGs"
+  type        = string
+}
