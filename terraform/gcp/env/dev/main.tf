1504c7812af2f08e5916791851ef4287f19c4b0b
rihab-lab <rihab.bahri7@gmail.com>
2026-02-24 09:32:24 +0100

    composer dev add roles

--- terraform/gcp/env/dev/main.tf
+++ terraform/gcp/env/dev/main.tf
@@ -240,3 +240,21 @@ resource "google_composer_environment" "dev" {
     }
   }
 }
+
+# Accès à l’UI Composer
+resource "google_project_iam_member" "composer_user_me" {
+  project = local.project_id
+  role    = "roles/composer.user"
+  member  = "user:rihab.bahri7@rbaapp.com"
+
+  depends_on = [google_project_service.apis]
+}
+
+# Accès Monitoring (sinon page surveillance bloquée)
+resource "google_project_iam_member" "monitoring_viewer_me" {
+  project = local.project_id
+  role    = "roles/monitoring.viewer"
+  member  = "user:rihab.bahri7@rbaapp.com"
+
+  depends_on = [google_project_service.apis]
+}
