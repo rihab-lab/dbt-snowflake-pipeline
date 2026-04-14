4b8f2c5fff8811885698e9dff56803df2e5bf9c0
rihab-lab <rihab.bahri7@gmail.com>
2026-04-14 10:15:23 +0200

    CREATE GCP STAGE

--- terraform/snowflake/envs/dev/main.tf
+++ terraform/snowflake/envs/dev/main.tf
@@ -14,3 +14,42 @@ module "snowflake_env" {
 
   svc_dbt_password = var.svc_dbt_password
 }
+#create snowflake stage
+resource "snowflake_storage_integration" "gcs_int" {
+  name    = "GCS_STORAGE_INT"
+  type    = "EXTERNAL_STAGE"
+  enabled = true
+
+  storage_provider = "GCS"
+
+  storage_allowed_locations = [
+    "gcs://bck-pipeone-landing-dev/"
+  ]
+}
+resource "snowflake_file_format" "csv_format" {
+  name     = "CSV_FORMAT"
+  database = "MYDATA_DEV"
+  schema   = "RAW"
+
+  format_type = "CSV"
+
+  skip_header = 1
+  field_optionally_enclosed_by = "\""
+  null_if = ["NULL", "null", ""]
+}
+resource "snowflake_stage" "gcp_stage" {
+  name     = "EXTERNAL_GCP_STAGE_STORAGE"
+  database = "MYDATA_DEV"
+  schema   = "RAW"
+
+  url = "gcs://bck-pipeone-landing-dev/"
+
+  storage_integration = snowflake_storage_integration.gcs_int.name
+
+  file_format = "FORMAT_NAME = MYDATA_DEV.RAW.CSV_FORMAT"
+
+  depends_on = [
+    snowflake_storage_integration.gcs_int,
+    snowflake_file_format.csv_format
+  ]
+}
