bb7efab1fc76c33cb44ad484b0db4b6833f78606
rihab-lab <rihab.bahri7@gmail.com>
2026-04-14 11:21:51 +0200

    CREATE GCP STAGE

--- terraform/snowflake/envs/dev/provider.tf
+++ terraform/snowflake/envs/dev/provider.tf
@@ -7,16 +7,16 @@ terraform {
   }
 }
 
-/*provider "snowflake" {
+provider "snowflake" {
   organization_name = var.snowflake_organization_name
   account_name      = var.snowflake_account_name
 
   user     = var.snowflake_username
   password = var.snowflake_password
   role     = "ACCOUNTADMIN"
-}*/
+}
 
-provider "snowflake" {
+/*provider "snowflake" {
   organization_name = var.snowflake_organization_name
   account_name      = var.snowflake_account_name
 
@@ -24,4 +24,4 @@ provider "snowflake" {
   password = var.snow_pwd
 
   role = "ACCOUNTADMIN"
-}
+}*/
